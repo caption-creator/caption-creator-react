f6cfb0a923f06a191319bd68b59b6e95949c3fa8
hanseungwoo <seungwoo.han@kscy.kr>
2021-09-23 00:30:51 +0900

    메인화면 스케치

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,17 +1,57 @@
 import React from 'react';
 import Container from '../components/Container';
 import styled from 'styled-components';
-
+import { Grid } from '@material-ui/core';
+import UploadContainer from '../components/UploadContainer';
 
 const Title = styled.p`
+  font-size: 16px;
+  color: #007AFF;
+  font-weight: 600;
   margin: 0px;
+  margin-bottom: 10px;
 `
 
-const Link = styled.p`
-  font-size: 14px;
-  font-weight: 600;
-  cursor: pointer;
+const MyContentWrapper = styled.div`
+  height: 500px;
+  overflow: auto;
+  margin-bottom: 20px;
 `
+
+const Hint = styled.p`
+  color: #FFFFFF;
+  font-size: 12px;
+  margin: 0px;
+  padding: 10px;
+`
+
+const GridBox = ({ color }) => {
+  const [size, setSize] = React.useState(0)
+  const boxRef = React.useRef()
+
+  React.useEffect(() => {
+    if(boxRef.current){
+      setSize(boxRef.current.offsetWidth)
+    }
+  }, [boxRef])
+
+  return (
+    <Grid 
+      item
+      xs={4} 
+      ref={boxRef}
+      style={{
+        width: '100%', 
+        height: size, 
+        backgroundColor: '#' + Math.round(Math.random() * 0xffffff).toString(16)
+      }}
+    >
+      <Hint>여기는 사진</Hint>
+    </Grid>
+  )
+}
+
+
 
 const MainPage = () => {
 
@@ -21,10 +61,15 @@ const MainPage = () => {
 
   return (
     <Container>
-      <Link onClick={() => to("/")}>홈</Link> 
-      <Link onClick={() => to("/")}>업로드</Link> 
-      <Link onClick={() => to("/")}>로그인</Link> 
-      <Link onClick={() => to("/")}></Link> 
+      <Title>여기에 본인 피드 내용이 들어가면 어떨까 싶어요</Title>
+      <MyContentWrapper>
+        <Grid container>
+          {"111111111111111111111".split("1").map((item, idx) => {
+            return <GridBox key={idx} color={item} />
+          })}
+        </Grid>
+      </MyContentWrapper>
+      <UploadContainer />
     </Container>
   )
 }
